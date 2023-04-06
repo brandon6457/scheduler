c8ded34102eae14798c8c1cc56ca19f6ec4783d3
brandon6457 <brandon6457@gmail.com>
2023-04-06 18:04:50 -0400

    fixed issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,9 @@ $(function () {
    })
 });
 $(document).ready(function () {
-
-   $(".saveBtn").on("click", function (e) {
-      e.preventDefault();
-      var text = $(this).siblings("description").val();
+   $(".saveBtn").on("click", function (event) {
+      event.preventDefault();
+      var text = $(this).siblings(".description").val();
       var time = $(this).parent().attr("id");
 
       localStorage.setItem(time, text);
@@ -32,10 +31,10 @@ window.onload = function loadContent() {
    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-   $("#hour-13 .description").val(localStorage.getItem("hour-1"));
-   $("#hour-14 .description").val(localStorage.getItem("hour-2"));
-   $("#hour-15 .description").val(localStorage.getItem("hour-3"));
-   $("#hour-16 .description").val(localStorage.getItem("hour-4"));
-   $("#hour-17 .description").val(localStorage.getItem("hour-5"));
+   $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+   $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+   $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+   $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 }
